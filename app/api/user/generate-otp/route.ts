96c50d73811f4c82fb9290d238c85188009e7865
abhii2003 <abhinavkush2003@gmail.com>
2024-09-11 13:22:22 +0530

    hopeful fix to nextauth for build v9 --abhi

--- app/api/user/generate-otp/route.ts
+++ app/api/user/generate-otp/route.ts
@@ -1,7 +1,7 @@
 import { randomBytes } from 'crypto';
 import { google } from 'googleapis';
 import nodemailer from 'nodemailer';
-import { prisma } from '@/lib/prisma';
+import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 // OAuth2 configuration for Gmail
