bdfe96f3bfee5e310dcd65a1c6de75178a2c93a6
abhii2003 <abhinavkush2003@gmail.com>
2024-09-16 17:07:25 +0530

    added content to about page -- abhi

--- app/(other-pages)/faculty/page.tsx
+++ app/(other-pages)/faculty/page.tsx
@@ -11,10 +11,21 @@ const About: React.FC = () => {
 
                 <h1 className="text-3xl font-bold font-cinzel text-barn_red text-center mb-8">ABOUT US</h1>
                 <p className="my-2 font-helvetica size-blockquote text-justify-center md:px-32">
-                    <i>
-                        Building a child is like building the Nation, F. I. College of Nursing was laid by Late Mr. Iqbal Ahmad sir, in his guidance F.I. College of Nursing
-                        encouraging the upcoming young generation and started developing the new strength in the upcoming nation builders.
-                    </i>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </p>
+                <p className="my-2 font-helvetica size-blockquote text-justify-center md:px-32">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </p>
+            </div>
+            <Image src="" alt="about image" className="h-96 w-11/12 max-w-screen-2xl mb-10" />
+            <div className="flex flex-col gap-2 p-0 max-w-screen-2xl text-xl items-center text-center">
+
+                <h1 className="text-3xl font-bold font-cinzel text-barn_red text-center mb-8">ABOUT US</h1>
+                <p className="my-2 font-helvetica size-blockquote text-justify-center md:px-32">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </p>
+                <p className="my-2 font-helvetica size-blockquote text-justify-center md:px-32">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                 </p>
             </div>
         </section>
