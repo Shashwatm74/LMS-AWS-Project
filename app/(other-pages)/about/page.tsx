4d1b57ebb2c5d5786af226ec00c1b5e26094bdce
abhii2003 <abhinavkush2003@gmail.com>
2024-09-16 17:16:26 +0530

    fixed the content of about -- abhi

--- app/(other-pages)/about/page.tsx
+++ app/(other-pages)/about/page.tsx
@@ -18,9 +18,7 @@ const About: React.FC = () => {
                 </p>
             </div>
             <Image src="" alt="about image" className="h-96 w-11/12 max-w-screen-2xl mb-10" />
-            <div className="flex flex-col gap-2 p-0 max-w-screen-2xl text-xl items-center text-center">
-
-                <h1 className="text-3xl font-bold font-cinzel text-barn_red text-center mb-8">ABOUT US</h1>
+            <div className="flex flex-col gap-2 p-0 max-w-screen-2xl text-xl items-center text-center text-barn_red">
                 <p className="my-2 font-helvetica size-blockquote text-justify-center md:px-32">
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                 </p>
