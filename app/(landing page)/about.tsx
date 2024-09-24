c902f7577b7414ba5555f9dbb8c9c47e47dfdfe3
abhii2003 <abhinavkush2003@gmail.com>
2024-09-24 21:30:57 +0530

    fixed the landing page -- abhi

--- app/(landing page)/about.tsx
+++ app/(landing page)/about.tsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 const About: React.FC = () => {
     return (
-        <section id="about" className="min-h-fit flex flex-row bg-barn_red p-10 py-7 text-white font-helvetica size-blockqoute">
-            <div className="w-1/2 flex flex-col">
-                <h1 className="font-bold font-cinzel text-3xl py-6">TRUSTED BY 10,000+ STUDENTS</h1>
-                <p className="mt-4 text-lg w-3/4 pb-6">
+        <section id="about" className="min-h-fit flex flex-col-reverse md:flex-row bg-barn_red p-10 py-7 text-white font-helvetica size-blockqoute ">
+            <div className="w-full md:w-1/2 flex flex-col pt-4 md:pt-0 items-center text-center md:items-start md:text-start">
+                <h1 className="font-bold font-cinzel text-3xl md:py-6">TRUSTED BY 10,000+ STUDENTS</h1>
+                <p className="mt-4 text-lg w-full md:w-3/4 pb-6">
                     Nursing is not for everyone. It takes a very strong, intelligent,
                     and compassionate person to take on the ills of the world with
                     passion and purpose and work to maintain the health
                     and well-being of the planet.
                 </p>
-                <p className="text-lg pt-2">WE PROVIDE THE BEST NURSING AND PARAMEDICAL COURSES</p>
+                <p className="text-lg pt-2 ">WE PROVIDE THE BEST NURSING AND PARAMEDICAL COURSES</p>
                 <div className="py-8 ">
                     <Button className=" w-40 rounded-none text-barn_red bg-white hover:text-white hover:bg-charcoal">VIEW COURSES</Button>
                 </div>
             </div>
-            <div className="w-1/2">
+            <div className="w-full md:w-1/2">
                 <Image src='' alt='about-page-image' className="h-96 w-100" />
             </div>
         </section>
