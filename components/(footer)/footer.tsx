751db0925fd18665719ec471d4cd64d574fe732d
Shashwat Mishra <shashwat.mishra2022@vitstudent.ac.in>
2024-10-22 21:39:19 +0530

    init --shash

--- components/(footer)/footer.tsx
+++ components/(footer)/footer.tsx
@@ -1,8 +1,11 @@
 // components/Footer.tsx
 export default function Footer() {
-    return (
-        <footer className=" text-white p-2 text-center" style={{ backgroundColor: '#7a0e01', color: 'white' }}>
-            <p>Copyright © 22BCE1746 and 22BCE1743</p>
-        </footer>
-    );
+  return (
+    <footer
+      className=" text-white p-2 text-center"
+      style={{ backgroundColor: "#7a0e01", color: "white" }}
+    >
+      <p>Copyright © Shashwat Mishra</p>
+    </footer>
+  );
 }
